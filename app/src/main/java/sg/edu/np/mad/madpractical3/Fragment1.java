fa3cb71b1482ca203bc0599f86c6fe989ae6d734
Tan Yu Lin <S10257915@connect.np.edu.sg>
2024-05-19 23:27:59 +0800

    Update of wk 3 practical until challenge Q9.

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/Fragment1.java
@@ -0,0 +1,46 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static Fragment1 newInstance(String param1) {
+        Fragment1 fragment = new Fragment1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
--- app/src/main/java/sg/edu/np/mad/madpractical3/MessageGroup.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MessageGroup.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.madpractical3;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,32 @@ public class MessageGroup extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Create instances of Fragment1 and Fragment2
+        final Fragment1 fragment1 = new Fragment1();
+        final Fragment2 fragment2 = new Fragment2();
+
+        // Set OnClickListener for "GROUP 1" button
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace current fragment with Fragment1
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.imageView, fragment1)
+                        .commit();
+            }
+        });
+
+        // Set OnClickListener for "GROUP 2" button
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace current fragment with Fragment2
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.imageView, fragment2)
+                        .commit();
+            }
         });
     }
 }
